0b0d15b27df3e2d0f4394d2321a945c6bad024b2
1dimadim1 <you@example.com>
2019-05-09 21:33:43 +0300

    bug fixed

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-window.onload = function() {
+window.onload = function () {
+
     var LastClick;
+
     Array.from(document.getElementsByTagName('td')).map(item => {
-        item.onclick = function() {
-            LastClick=this;
-            document.querySelector(".table2").style.visibility = "visible";
+        item.onclick = function () {
+            if ('rgb(255, 253, 208)' != this.style.backgroundColor) {
+                LastClick = this;
+                document.querySelector(".table2").style.visibility = "visible";
+            }
+
         };
     });
+
     Array.from(document.getElementsByTagName('th')).map(item => {
-        item.onclick = function() {
-            LastClick.innerText=this.innerText;
+        item.onclick = function () {
+            LastClick.innerText = this.innerText;
             document.querySelector(".table2").style.visibility = "hidden";
+            if (LastClick.innerText == 0) {
+                LastClick.style.backgroundColor = 'rgb(255, 248, 220)';
+            }
+            else{
+                LastClick.style.backgroundColor = 'rgb(242, 223, 147)';
+            }
         };
+    });
+
+    var array = [];
+    $('.button2').on('click', function () {
+        $('tr', 'table.table1').each(function () {
+            var str = "";
+            $(this).find('td').each(function () {
+                str = str + ($(this).html());
+            });
+            array.push(str);
+        });
+        console.log(array);
+        $.ajax({
+            type: "POST",
+            url: "Check.php",
+            data: {"array": array},
+            success: function (data) {
+                alert(data);
+            }
+        });
+        array = []
     });
 }
+
--- update.php
+++ update.php
@@ -1,8 +1,15 @@
 <?php
 $level = $_POST["level"];
-$h = fopen("Fields.txt","r");
-$arr = file ("Fields.txt");
-fclose($h);
+$conn = new mysqli('localhost', 'root', '', 'sudoku');
+$query = "SELECT `Field` FROM `fields` WHERE `ID`=$level";
+$res = mysqli_query($conn, $query);
+$resS =mysqli_fetch_row($res);
+$arr=explode("
+",$resS[0]);
+if ($level <= 0 || $level > 12) {
+    header("Location: ./index.html");
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,31 +17,29 @@ fclose($h);
     <meta charset="UTF-8">
     <title>Sudoku</title>
     <link rel="stylesheet" href="./style.css">
+    <script type="text/javascript" src="jquery-3.4.1.js"></script>
     <script src="./index.js"></script>
 </head>
 <body>
-
 <header>
     Судоку
 </header>
 <div class="container1">
-    Выберите поле
-
-    <table border="1" class="table1">
+    Уровень № <?php echo $level;?>
+    <table border="1" class="table1" id="table1">
         <caption></caption>
         <?php
-        foreach($arr as $i => $a)
-        {
+        foreach ($arr as $i => $a) {
             echo "<tr>";
-            for($z=0;$z<9;$z++){
-                if(mb_substr($a, $z, 1)==0)
-                    echo "<td>",mb_substr($a, $z, 1),"</td>";
+            for ($z = 0; $z < 9; $z++) {
+                if (mb_substr($a, $z, 1) == 0)
+                    echo "<td>", mb_substr($a, $z, 1), "</td>";
                 else
-                    echo "<td style='unchangeable'>",mb_substr($a, $z, 1),"</td>";
+                    echo "<td style='background-color: #fffdd0;'>", mb_substr($a, $z, 1), "</td>";
             }
             echo "</tr>";
         }
-   ?>
+        ?>
     </table>
     <table border="1" class="table2">
         <tr>
@@ -72,12 +77,9 @@ fclose($h);
 <div class="container2">
     <form action="./update.php" method=POST>
         <input type="number" name="level" value="1" required>
-        <button type="submit">Обновить</button>
+        <button type="submit" class="button1">Обновить</button>
     </form>
-    <form action="Check.php" method=POST>
-        <button type=submit>Проверить</button>
-    </form>
-
+    <button type=submit class="button2">Проверить</button>
 </div>
 <footer>Made by Dmitry Lis</footer>
 </body>
